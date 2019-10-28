ef31598d180129dc1dafb98f469675a6ba34730c
Jon Betts <jon.betts@hypothes.is>
2019-10-28 12:05:30 +0000

    Formatting issues

--- src/h_matchers/__init__.py
+++ src/h_matchers/__init__.py
@@ -1,6 +1,6 @@
 """Exposes the user facing matchers for importing"""
 
-from h_matchers.code import AnyFunction, AnyInstanceOf, AnyCallable
+from h_matchers.code import AnyCallable, AnyFunction, AnyInstanceOf
 from h_matchers.core import Matcher
 from h_matchers.string import AnyString
 
