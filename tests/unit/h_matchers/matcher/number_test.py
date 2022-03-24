48044ff6a7b48fa0d120d0d6b0a94c551f8a1675
Jon Betts <jon.betts@hypothes.is>
2022-03-24 19:35:09 +0000

    Add lots of numeric comparators

--- tests/unit/h_matchers/matcher/number_test.py
+++ tests/unit/h_matchers/matcher/number_test.py
@@ -1,7 +1,46 @@
 import pytest
 
-from h_matchers.matcher.number import AnyComplex, AnyFloat, AnyInt
+from h_matchers.matcher.number import (
+    AnyComplex,
+    AnyDecimal,
+    AnyFloat,
+    AnyInt,
+    AnyNumber,
+    AnyReal,
+)
 from tests.unit.data_types import DataTypes
+
+
+class TestAnyNumber:
+    @pytest.mark.parametrize(
+        "item,_", DataTypes.parameters(exact=DataTypes.Groups.NUMERIC)
+    )
+    def test_it_matches(self, item, _):
+        assert AnyNumber() == item
+        assert item == AnyNumber()
+
+    @pytest.mark.parametrize(
+        "item,_", DataTypes.parameters(exclude=DataTypes.Groups.NUMERIC)
+    )
+    def test_it_does_not_match(self, item, _):
+        assert AnyNumber() != item
+        assert item != AnyNumber()
+
+
+class TestAnyReal:
+    @pytest.mark.parametrize(
+        "item,_", DataTypes.parameters(exact=DataTypes.Groups.REALS)
+    )
+    def test_it_matches(self, item, _):
+        assert AnyReal() == item
+        assert item == AnyReal()
+
+    @pytest.mark.parametrize(
+        "item,_", DataTypes.parameters(exclude=DataTypes.Groups.REALS)
+    )
+    def test_it_does_not_match(self, item, _):
+        assert AnyReal() != item
+        assert item != AnyReal()
 
 
 class TestAnyInt:
@@ -50,3 +89,19 @@ class TestAnyComplex:
     def test_it_does_not_match(self, item, _):
         assert AnyComplex() != item
         assert item != AnyComplex()
+
+
+class TestAnyDecimal:
+    @pytest.mark.parametrize(
+        "item,_", DataTypes.parameters(exact=DataTypes.Groups.DECIMAL)
+    )
+    def test_it_matches(self, item, _):
+        assert AnyDecimal() == item
+        assert item == AnyDecimal()
+
+    @pytest.mark.parametrize(
+        "item,_", DataTypes.parameters(exclude=DataTypes.Groups.DECIMAL)
+    )
+    def test_it_does_not_match(self, item, _):
+        assert AnyDecimal() != item
+        assert item != AnyDecimal()
